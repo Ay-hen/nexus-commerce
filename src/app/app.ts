4b9ef40ac89843ce2a88b0b240a1706dc5e4c555
Ayoub Brahim Hennani <hennani.2020@gmail.com>
2026-06-15 16:09:59 +0100

    navbar v1.1 + home

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Navbar } from "./components/navbar/navbar";
+import { Home } from "./components/home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [Home],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
