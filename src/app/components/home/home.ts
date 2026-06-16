0e2136ed3b0594e5e18f27c7b09646b22dde6124
Ayoub Brahim Hennani <hennani.2020@gmail.com>
2026-06-16 11:26:55 +0100

    home updates + navbar

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
-import { Navbar } from "../navbar/navbar";
+import { CommonModule } from '@angular/common';
+import { Navbar } from '../navbar/navbar';
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar],
+  standalone: true,
+  imports: [CommonModule, Navbar],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {}
+export class Home {
+
+  icons = {
+    shield: '/icons/shield.png',
+    truck: '/icons/truck.png',
+    headset: '/icons/headset.png'
+  };
+
+  categories = [
+    {
+      name: 'Laptops',
+      desc: 'High performance for every need',
+      image: '/categories/laptop.jfif'
+    },
+    {
+      name: 'Audio',
+      desc: 'Premium sound experience',
+      image: '/categories/headphones.png'
+    },
+    {
+      name: 'Wearables',
+      desc: 'Technology that moves with you',
+      image: '/categories/watch.png'
+    },
+    {
+      name: 'Smartphones',
+      desc: 'Innovation in your hands',
+      image: '/categories/phone.png'
+    },
+    {
+      name: 'Clothes',
+      desc: 'Trendy fashion for every style',
+      image: '/categories/clothes.png'
+    }
+  ];
+
+}
