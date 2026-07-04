bc8c5dfc96bee2a6a6ef52e825533dfd19225719
Ayoub Brahim Hennani <hennani.2020@gmail.com>
2026-07-04 11:22:10 +0100

    add inventory item

---
+++ src/app/admin/components/add-inventory-item/add-inventory-item.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-inventory-item',
+  imports: [],
+  templateUrl: './add-inventory-item.html',
+  styleUrl: './add-inventory-item.scss',
+})
+export class AddInventoryItem {}
