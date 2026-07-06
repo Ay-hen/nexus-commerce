4280463a1f1bdd472d3001750053539730f3e30b
Ayoub Brahim Hennani <hennani.2020@gmail.com>
2026-07-06 13:17:21 +0100

    notification dropdown component

---
+++ src/app/admin/components/notification-dropdown/notification-dropdown.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification-dropdown',
+  imports: [],
+  templateUrl: './notification-dropdown.html',
+  styleUrl: './notification-dropdown.scss',
+})
+export class NotificationDropdown {}
