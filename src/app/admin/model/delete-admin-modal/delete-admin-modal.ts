a5693509d6262909b979a8c7f6108cf3f9bef0c2
Ayoub Brahim Hennani <hennani.2020@gmail.com>
2026-07-10 12:50:36 +0100

    delete admin modal component

---
+++ src/app/admin/model/delete-admin-modal/delete-admin-modal.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-admin-modal',
+  imports: [],
+  templateUrl: './delete-admin-modal.html',
+  styleUrl: './delete-admin-modal.scss',
+})
+export class DeleteAdminModal {}
