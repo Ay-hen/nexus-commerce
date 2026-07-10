953612efcbe649220973721765ce2c07dc93319b
Ayoub Brahim Hennani <hennani.2020@gmail.com>
2026-07-10 12:55:44 +0100

    view admin model component

---
+++ src/app/admin/model/view-admin-modal/view-admin-modal.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-admin-modal',
+  imports: [],
+  templateUrl: './view-admin-modal.html',
+  styleUrl: './view-admin-modal.scss',
+})
+export class ViewAdminModal {}
