4ba03e7c1b3ed390c24dcb83125cf3ade897eec7
Ayoub Brahim Hennani <hennani.2020@gmail.com>
2026-07-10 12:54:40 +0100

    add admin component

---
+++ src/app/admin/model/add-admin-modal/add-admin-modal.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-admin-modal',
+  imports: [],
+  templateUrl: './add-admin-modal.html',
+  styleUrl: './add-admin-modal.scss',
+})
+export class AddAdminModal {}
