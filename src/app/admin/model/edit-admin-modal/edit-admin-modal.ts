ccb9c9a0e9c0972b9833bd010a69a602ba3565f8
Ayoub Brahim Hennani <hennani.2020@gmail.com>
2026-07-10 12:52:26 +0100

    edit admin modal component

---
+++ src/app/admin/model/edit-admin-modal/edit-admin-modal.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-admin-modal',
+  imports: [],
+  templateUrl: './edit-admin-modal.html',
+  styleUrl: './edit-admin-modal.scss',
+})
+export class EditAdminModal {}
