969023cd0e44a8fef3803eab4f8550956e056882
Ayoub Brahim Hennani <hennani.2020@gmail.com>
2026-07-13 01:09:28 +0100

    view activity model component

---
+++ src/app/admin/model/view-activity-modal/view-activity-modal.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-activity-modal',
+  imports: [],
+  templateUrl: './view-activity-modal.html',
+  styleUrl: './view-activity-modal.scss',
+})
+export class ViewActivityModal {}
