03b6db7aae8baa5c8e9d617c0c60ec86958a898f
Ayoub Brahim Hennani <hennani.2020@gmail.com>
2026-09-01 15:13:28 +0100

    feat(email-settings):  component TypeScript logic updated

--- src/app/admin/components/sections/settings/email-settings/email-settings.ts
+++ src/app/admin/components/sections/settings/email-settings/email-settings.ts
@@ -2,8 +2,8 @@ import { Component, EventEmitter, Input, Output, signal, inject } from '@angular
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { EmailSettings, SmtpEncryption } from '../../../../model/settings.model';
-import { LanguageService } from '../../../../localization/language.service';
-import { TranslatePipe } from '../../../../localization/translate.pipe';
+import { LanguageService } from '../../../../../localization/language.service';
+import { TranslatePipe } from '../../../../../localization/translate.pipe';
 
 const EMAIL_RE = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
