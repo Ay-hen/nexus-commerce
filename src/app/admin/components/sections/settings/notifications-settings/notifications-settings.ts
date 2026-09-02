9ac48d7d87ec0ddcdfafe0d5c2c7e8f54c19355b
Ayoub Brahim Hennani <hennani.2020@gmail.com>
2026-09-02 18:35:23 +0100

    feat(notifications-settings): component created

---
+++ src/app/admin/components/sections/settings/notifications-settings/notifications-settings.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notifications-settings',
+  imports: [],
+  templateUrl: './notifications-settings.html',
+  styleUrl: './notifications-settings.scss',
+})
+export class NotificationsSettings {}
